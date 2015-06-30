53155eed545ea6b05a27f3bcfba4249ce3f561c5
jochaz <jordan.chariot@gmail.com>
2015-06-30 09:46:13 +0200

    Ajout de la fonctionnalité de recherche par critère

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -2,6 +2,7 @@ var app = angular.module("geektic", ['ngRoute']);
 
 app.config(['$routeProvider',
     function($routeProvider) {
+		//Définition de routeprovider
         $routeProvider.
             when('/', {
                 templateUrl: 'welcome.html',
@@ -20,28 +21,38 @@ app.config(['$routeProvider',
             });
     }]);
 
-app.controller('SearchCtrl', function($scope, $http) {
-    $http.get('api/interets').success(function(interets) {
-        $scope.lesInterets = interets;
-    });
+app.controller('SearchCtrl', function($scope, $http) {   
+	//Fonction retournant la liste des geeks en fonction de l'interet et du sexe choisi 
+    $scope.search = function(item, event){
+    	$http.get('api/geeks/', 
+    			{params: {"sexe":$scope.sexe, "interet": $scope.interet}}
+    			).success(function(lesGeeks) {
+    				$scope.lesGeeks = lesGeeks;
+    	}).error(function(){
+    		console.log("Erreur");
+    	});
 
+    }
+    
+    //Retourne les interets (pour la combobox)
+	$http.get('api/interets').success(function(interets) {
+        $scope.lesInterets = interets;
+        
+    });
+    
+    
 });
 
-app.controller('SearchCtrl', function($scope, $http, $routeParams) {
-    $http.get('api/geeks').success(function(lesGeek) {
-        $scope.lesInterets = interets;
-    });
 
-});
-
+//USELESS
 app.controller('IndexCtrl', function($scope, $http) {
-    $http.get('/api/hello').success(function(helloMessage) {
+    $http.get('api/hello').success(function(helloMessage) {
         $scope.helloMessage = helloMessage;
     });
 });
 
 
-
+//Retourne un geek en fonction de son ID
 app.controller('GeeksCtrl', function($scope, $http, $routeParams) {
     $http.get('api/geeks/' + $routeParams.id).success(function(leGeek) {
         $scope.leGeek = leGeek;
